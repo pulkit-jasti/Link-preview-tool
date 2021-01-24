2bc13fceed667e65ee4c4839a632f798ebb29fb7
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-01-25 01:54:46 +0530

    Project complete

--- server.js
+++ server.js
@@ -13,25 +13,17 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.render('index', {
 		objj: [],
 	});
-});*/
+});
 
-//Test URLs
-// https://www.investopedia.com/articles/wealth-management/121415/economics-owning-small-plane.asp https://theblondeabroad.com/best-places-to-travel-in-the-united-states-right-now/    https://www.investopedia.com/guide-to-the-joe-biden-presidency-5095912 h/beebom.com/sony-sold-twice-playstation-5-units-than-xbox-series-x-2020/  https://fireship.io/courses/vue/ https://fireship.io/courses/angular/
+app.post('/', (req, res) => {
+	console.log(req.body);
+	const data = req.body;
 
-app.get('/', (req, res) => {
-	//console.log(req.body);
-	//const data = req.body;
-
-	//const URLs = Array.from(getURLs(data.URLs));
-	const URLs = Array.from(
-		getURLs(
-			'https://www.theverge.com/22158504/best-games-2020-ps5-xbox-nintendo-tlou2-animal-crossing-miles-morales https://www.androidpolice.com/2021/01/22/microsoft-edges-latest-update-adds-new-themes-sleeping-tabs-and-more/   https://theblondeabroad.com/the-ultimate-guide-to-solo-female-travel/ '
-		)
-	);
+	const URLs = Array.from(getURLs(data.URLs));
 
 	async function getMeta(URL) {
 		let response = await fetch(URL);
@@ -42,14 +34,12 @@ app.get('/', (req, res) => {
 		function getIcon() {
 			const iconFetch = $("link[rel='shortcut icon']").attr('href') || $("link[rel='icon']").attr('href');
 
-			//if (iconFetch.slice(0, 8) == 'https://' || iconFetch.slice(0, 3) == 'www') {
 			if (iconFetch.includes('https') || iconFetch.includes('www')) {
 				return iconFetch;
 			} else {
 				var domain = 'https://' + URL.replace('http://', '').replace('https://', '').split(/[/?#]/)[0];
 				return domain + '/' + iconFetch;
 			}
-			//return iconFetch.slice(0, 3);
 		}
 
 		const getTagData = propName =>
